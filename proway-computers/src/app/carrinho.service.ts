82d2bf2dfd63b889bd818877cef0b95957e4a16d
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-12-19 20:14:35 -0300

    service de ações do carrinho

--- proway-computers/src/app/carrinho.service.ts
+++ proway-computers/src/app/carrinho.service.ts
@@ -1,3 +1,4 @@
+import { IProdutoCarrinho } from './produtos';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class CarrinhoService {
 
+  itens: IProdutoCarrinho[] = [];
+
   constructor() { }
+
+  obtemCarrinho(){
+    return JSON.parse(localStorage.getItem("carrinho") || "");
+  }
+
+  adicionarAoCarrinho(produto: IProdutoCarrinho){
+    this.itens.push(produto);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens))
+  }
+
+  limparCarrinho(){
+    this.itens = [];
+    localStorage.clear();
+  }
 }
