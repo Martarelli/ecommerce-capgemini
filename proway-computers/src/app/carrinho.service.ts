b800277f896ce61989845368ba46a609593fa610
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-12-19 20:34:59 -0300

    alteração do retorno do metodo

--- proway-computers/src/app/carrinho.service.ts
+++ proway-computers/src/app/carrinho.service.ts
@@ -11,7 +11,8 @@ export class CarrinhoService {
   constructor() { }
 
   obtemCarrinho(){
-    return JSON.parse(localStorage.getItem("carrinho") || "");
+    this.itens = JSON.parse(localStorage.getItem("carrinho") || "");
+    return this.itens;
   }
 
   adicionarAoCarrinho(produto: IProdutoCarrinho){
