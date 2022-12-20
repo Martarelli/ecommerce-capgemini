ea3fd7eca67ecc2f61dfae757693ba30ba02d33f
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-12-19 21:00:50 -0300

    implementação do carrinho concluida

--- proway-computers/src/app/carrinho/carrinho.component.ts
+++ proway-computers/src/app/carrinho/carrinho.component.ts
@@ -1,6 +1,8 @@
+import { NotificacaoService } from './../notificacao.service';
 import { IProdutoCarrinho } from './../produtos';
 import { CarrinhoService } from './../carrinho.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -10,13 +12,33 @@ import { Component, OnInit } from '@angular/core';
 export class CarrinhoComponent implements OnInit {
 
   itensCarrinho: IProdutoCarrinho[] = [];
+  total: number = 0;
 
   constructor(
-    public carrinhoService: CarrinhoService
+    public carrinhoService: CarrinhoService,
+    private router: Router,
+    private notificacaoService: NotificacaoService
   ) { }
 
   ngOnInit(): void {
     this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calcularTotal();
+  }
+
+  calcularTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr)=> prev + (curr.preco * curr.quantidade), 0);
+  }
+
+  removerProdutoCarrinho(produtoId: number){
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
+  }
+
+  comprar(){
+    this.notificacaoService.notificar("Parabéns, você finalizou a compra");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
   }
 
 }
